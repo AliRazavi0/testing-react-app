fa48f37d95cf55deaa09714746505a3860cab170
AliRazavi0 <razavi.ali.1998@gmail.com>
2022-03-15 02:50:56 +0330

    feat: act method test component

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,22 @@
 import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 import App from "./App";
 
 describe("App Component Test", () => {
   it("should display Hello Test", () => {
+    //arange
     const container = document.createElement("div");
     document.body.appendChild(container);
+    //act
+    act(() => {
+      render(<App />, container);
+    });
 
-    render(<App />, container);
-
+    //assert
     expect(container.textContent).toBe("Hello Test");
 
     unmountComponentAtNode(container); // remove element dom
   });
 });
+
+// ACT => Full Load Document
